ca16cdb20260feba2ad3576f423da54d7f8226c3
doukaiwei <2200600836@qq.com>
2024-10-31 16:15:41 +0800

    集成eslint插件

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    // 配置项
+    env: {
+        browser: true, // 浏览器环境
+        es2021: true, // ES2021 特性
+        node: true, // Node.js 环境
+    },
+    parserOptions: {
+        ecmaVersion: 'latest', // 使用最新的 ECMAScript 版本
+        sourceType: 'module', // 使用 ES6 模块语法
+        ecmaFeatures: {
+            jsx: true, // 启用 JSX（如果适用，比如 React）
+        },
+        // 如果使用 TypeScript 解析器，可以添加以下配置：
+        parser: '@typescript-eslint/parser',
+        // project: './tsconfig.json', // 指定 tsconfig.json 文件的位置（如果适用）
+    },
+    // 指定要使用的插件
+    plugins: [
+        '@typescript-eslint', // TypeScript 插件（如果适用）
+    ],
+};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// index.tsx
+import "./publicPath";//导入上一步配置的文件，用于正确加载静态资源文件
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export async function bootstrap() {
+    console.log("[react16] react app bootstraped");
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+export async function mount(props: any) {
+    console.log("[react16] props from main framework", props);
+    ReactDOM.render(
+        <BrowserRouter
+            basename={window.__POWERED_BY_QIANKUN__ ? "tools_web" : undefined}
+        >
+            <App />
+        </BrowserRouter>,
+        props.container
+            ? props.container.querySelector("#root")
+            : document.getElementById("root")
+    );
+}
+
+export async function unmount(props: any) {
+    const { container } = props;
+    ReactDOM.unmountComponentAtNode(
+        container
+            ? container.querySelector("#root")
+            : document.querySelector("#root")
+    );
+}
+// @ts-ignore
+if (!window.__POWERED_BY_QIANKUN__) {
+    ReactDOM.render(
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>,
+        document.getElementById("root")
+    );
+}
 reportWebVitals();
