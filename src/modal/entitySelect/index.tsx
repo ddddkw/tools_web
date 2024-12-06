dede2a16d07ab511140af476375734ada520e830
doukaiwei <2200600836@qq.com>
2024-12-06 14:50:57 +0800

    table组件增加

--- src/modal/entitySelect/index.tsx
+++ src/modal/entitySelect/index.tsx
@@ -1,9 +1,66 @@
-import {Modal} from "antd";
+import {Modal, Table, TableProps , message } from "antd";
+import http from "../../utils/request";
+import { useEffect, useState} from "react";
+import Store from "../../store";
+import {getComById} from "../../utils/nodeUtils";
 
-export default function EntitySelect(){
+export default function EntitySelect(props: any) {
+    const [messageApi, contextHolder] = message.useMessage();
+    const {openModal, setOpenModal} = props
+    const [dataSource, setDataSource] = useState<any>();
+    const [selectedRowKeys, setSelectedRowKeys] = useState<any>([]);
+    const comList = JSON.parse(JSON.stringify(Store.getState().comList))
+    const selectCom = Store.getState().selectCom // 获取选中的节点
+    const selectNode = getComById(selectCom, comList) // 获取选中的节点
+    const queryAllEntity = function () {
+        http.get("beans/tables/getTables")
+            .then((res: any) => {
+                const middleData:any= []
+                res.data.forEach((item: any, index: any)=>{
+                    middleData.push({key:index,entityName:item})
+                })
+                setDataSource(middleData)
+            })
+    }
+    const onSelectChange = (newSelectedRowKeys:any) => {
+        setSelectedRowKeys(newSelectedRowKeys ? newSelectedRowKeys : [])
+    };
+    const queryTableColumns = function (val:string){
+        http.get("beans/tables/getColumns",{tableName:val})
+            .then((res: any) => {
+                selectNode.schemaList = res.data
+                selectNode.tableName = val
+                Store.dispatch({type: 'changeComList', value:comList})
+                setOpenModal(false)
+            })
+    }
+    const selectOk = function (){
+        if (rowSelection.selectedRowKeys.length>1) {
+            message.warning('选择一条数据后进行操作');
+        } else {
+            const tableName  = dataSource[rowSelection.selectedRowKeys[0]].entityName
+            queryTableColumns(tableName)
+        }
+    }
+    useEffect(()=>{
+        queryAllEntity()
+    },[]) // 加上[]是让useEffect只在组件挂载的时候运行一次
+    const rowSelection = {
+        selectedRowKeys,
+        onChange: onSelectChange,
+    };
+
+    const columns = [
+        {
+            title: '实体类',
+            dataIndex: 'entityName',
+            key: 'entityName',
+        },
+    ];
+
     return(
-        <Modal>
-            1
+        <Modal open={openModal} onCancel={()=>{setOpenModal(false)}} onOk={()=>{selectOk()}}>
+            <Table rowSelection={rowSelection} dataSource={dataSource} columns={columns} />
         </Modal>
     )
 }
--- src/views/leftPart/components/showDataCom/table/index.tsx
+++ src/views/leftPart/components/showDataCom/table/index.tsx
@@ -3,34 +3,48 @@ import { Table as AntTable } from 'antd';
 import http from "../../../../../utils/request";
 
 export default function Table(props: any){
-    const { entityCode, schemaList, size, bordered, showHeader = true, pagination = false, comStyle } = props;
+    const { tableName, schemaList, size, bordered, showHeader = true, pagination = false, comStyle } = props;
     const [entityData, setEntityData] = useState([])
 
     useEffect(() => {
-        if(entityCode) {
-            http.post("http://localhost:4000/entity/getEntityData", {entityCode})
+        if(tableName) {
+            http.get("beans/tables/queryTableData", {tableName})
                 .then((res:any) => {
-                    if(res.data.data) {
-                        setEntityData(res.data.data)
+                    if(res.data) {
+                        setEntityData(res.data)
                     }
                 })
         }
-    }, [entityCode])
+    }, [tableName])
 
     const getColumns = () => {
-        return (schemaList || []).map((item: any) => {
-            return {
-                title: item,
-                dataIndex: item,
-                key: item
+        return (schemaList || []).map((item: any,index:any) => {
+            if (item.fieldName ==='image') {
+                return {
+                    title: item.fieldName,
+                    dataIndex: item.fieldName,
+                    key: index,
+                    render:getImagePreview
+                }
+            } else {
+                return {
+                    title: item.fieldName,
+                    dataIndex: item.fieldName,
+                    key: index
+                }
             }
+
         })
     }
-
+    const getImagePreview=function (text:any,record:any){
+        return (
+            <img style={{width:'40px',height:'40px'}} src={`data:image/jpeg;base64,${record.image}`}></img>
+        )
+    }
     const getData = () => {
-        return entityData.map((item: any) => {
+        return entityData.map((item: any,index:any) => {
             return {
-                key: item._id,
+                key: index,
                 ...item
             }
         })
